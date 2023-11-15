6cc8eba142a74ff202eb53bb2dd442b3fa8ae8e4
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-11-14 20:24:25 -0500

    Privisional

---
+++ src/app/feature/vista-paciente/info-paciente-paciente/calendario-citas/cita-unit/cita-unit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cita-unit',
+  templateUrl: './cita-unit.component.html',
+  styleUrls: ['./cita-unit.component.css']
+})
+export class CitaUnitComponent {
+  labelUnit: string = "Hello Wolrd ";
+
+}
